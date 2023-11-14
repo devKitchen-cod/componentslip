18202d34f1e3d304a95c93731bdf878cc71c1162
Sergey <smagometov649@gmail.com>
2023-11-14 19:08:37 +0200

    add request

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,22 @@
-import style from '@/components/Button/button.module.css'
+'use client';
+import style from "@/components/Button/button.module.css";
+import axios from "axios";
 
-
-interface IProps{
-    text: string
+interface IProps {
+  text: string;
 }
 
-export default function Button({text}: IProps) {
-    return (
-        <div>
-            <button className={style.btnStyle}>{text}</button>
-        </div>
-    )
+export default function Button({ text }: IProps) {
+
+  const handleClick = async () => {
+    const res = await axios.get("http://localhost:8080/getAllAirports");
+    console.log('res.data', res.data);
+  };
+  return (
+    <div>
+      <button className={style.btnStyle} onClick={handleClick}>
+        {text}
+      </button>
+    </div>
+  );
 }
